1e78c42b8de29ece9a9f494c731deb73e7c6faca
amin-khorram <amin.khorram.ca@gmail.com>
2024-07-21 20:43:05 -0400

    comment

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             </div>
             <div>
                 <p className="text-xl mt-20">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Excepturi maiores dolorem reprehenderit voluptatem beatae, veniam saepe consequatur minus nemo culpa quia itaque et eius perferendis quibusdam accusantium architecto suscipit adipisci! Doloribus ullam qui vitae earum obcaecati. Culpa, reprehenderit repudiandae rem odit nostrum accusantium harum, doloremque perspiciatis, molestiae quae ipsam consequuntur!
+                    I am a Data Scientist.
                 </p>
                 <p className="text-xl">
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, pariatur unde perspiciatis voluptates iste fugit corrupti? At praesentium repellendus sint quas blanditiis quaerat voluptate a ipsa libero quae culpa ipsam iure, minus harum, necessitatibus tempore tempora cupiditate aliquam pariatur omnis ducimus consectetur. Eum ratione aut deserunt pariatur voluptatum impedit totam?
